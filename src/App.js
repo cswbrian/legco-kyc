27666417df1d5b04a0f88ffa8fec31ebe0438ff4
Brian Leung <brianleung1017@gmail.com>
2020-05-09 02:41:44 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,126 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import * as csv from "csvtojson";
+import theme from './themes'
+import { ThemeProvider } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Avatar from '@material-ui/core/Avatar';
+import styled from 'styled-components';
+
+
+const useFetch = (url, options) => {
+  const [response, setResponse] = useState(null);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(url, options);
+        const text = await res.text();
+        csv().fromString(text).then(json => setResponse(json));
+      } catch (error) {
+        setError(error);
+      }
+    };
+    fetchData();
+  }, []);
+  return { response, error };
+};
+
+const groupAnswers = array => {
+  return array.map(arr => {
+    let pureAnswer = {
+      ...arr
+    }
+
+    const ignoreKeys = ['question', 'question_title', 'description_text']
+    ignoreKeys.forEach(k => delete pureAnswer[k])
+
+    let answers = {}
+
+    for (let [key, value] of Object.entries(pureAnswer)) {
+      if (!answers[value]) {
+        answers[value] = [key]
+      } else {
+        answers[value].push(key)
+      }
+    }
+
+    return {
+      ...arr,
+      answers
+    }
+  })
+};
+
+const Group = styled(Grid)`
+  text-align: center;
+`
+const AvatarContainer = styled(Grid)`
+  .avatar {
+    width: 64px;
+    height: 64px;
+  }
+
+  .name {
+    margin: ${theme.spacing(1)}px;
+  }
+`
+
+const AvatarChart = props => {
+  const { answers } = props
+  return <Grid container justify='space-around'>
+    {Object.entries(answers).map(pair => {
+      const [label, ppl] = pair
+      return <Group item>
+        <Typography variant="h5" gutterBottom>
+          {ppl.length}
+        </Typography>
+        <Typography variant="h6" gutterBottom>
+          {label}
+        </Typography>
+        <Grid container>
+          {ppl.map(p => <AvatarContainer item>
+            <Grid container direction='column' alignItems='center'>
+              <Avatar className='avatar' alt={p} src="/static/images/avatar/1.jpg" />
+              <span className='name'>{p}</span>
+            </Grid>
+          </AvatarContainer>)}
+        </Grid>
+      </Group>
+    })}
+  </Grid>
+}
+
+
 
 function App() {
+  const res = useFetch(`https://docs.google.com/spreadsheets/d/e/2PACX-1vTDHXuMnpG9pwpg4nmDzzT0uLeXwx6DsoKmbmDP0Md-ljfmngZk54ldm8drFhpyqdMQeChwN5Te0TCS/pub?gid=2129939499&single=true&output=csv`, {});
+  if (!res.response) {
+    return <div>Loading...</div>
+  }
+
+
+  const data = groupAnswers(res.response)
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ThemeProvider theme={theme}>
+      <Container maxWidth="lg">
+    {data.map(dat => {
+      return <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Typography variant="h6" gutterBottom>{dat.question_title}</Typography>
+          <Typography variant="body1" paragraph>{dat.description_text}</Typography>
+          <AvatarChart answers={dat.answers} />
+        </Grid>
+      </Grid>
+    })}
+
+  </Container>
+  </ThemeProvider>
+    )
 }
 
 export default App;
