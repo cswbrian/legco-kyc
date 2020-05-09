565f40c3c30f28dfccf5133da4b9c2a0c357f868
Brian Leung <brianleung1017@gmail.com>
2020-05-09 13:33:12 +0800

    fix css file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import './App.css';
 import * as csv from "csvtojson";
 import theme from './themes'
 import { ThemeProvider } from '@material-ui/core/styles';
